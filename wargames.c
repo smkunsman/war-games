2a93f216c2da05a60aef244079150810ff7a69ec
Selena Kunsman <148573789+smkunsman@users.noreply.github.com>
2024-09-18 14:07:05 -0400

    color function

--- wargames.c
+++ wargames.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
+// method to make printf output blue
+void print_blue(const char *text) {
+    printf("\033[38;5;153m%s\033[0m", text);
+}
+
 int main (void) {  
 
     char login[30];
     do {
-        printf("Login: ");
+        print_blue("Login: ");
         scanf("%s", login);
         
         if (strcmp(login, "Joshua") != 0){
-            printf("Access Denied\n");
+            printf("\033[1;31mACCESS DENIED\033[0m\n");
         }
     } while(strcmp(login, "Joshua") != 0);
 
-    printf("\033[1;36mGREETINGS PROFESSOR FALKEN.\033[0m\n");
+    print_blue("GREETINGS PROFESSOR FALKEN.");
 }
