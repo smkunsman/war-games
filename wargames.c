f1c6760b7db815eb6d20aa08d546521c57109f1c
Selena <s.kunsman@setonhill.edu>
2024-09-18 14:01:26 -0400

    global blue

--- wargames.c
+++ wargames.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
+// method to make printf output blue
+void print_blue(const char *text) {
+    printf("\033[38;5;153m%s\033[0m", text);
+}
+
 int main (void) {  
 
     char login[30];
     do {
-        printf("Login: ");
+        print_blue("Login: ");
         scanf("%s", login);
         
         if (strcmp(login, "Joshua") != 0){
-            printf("Access Denied\n");
+            printf("\033[1;31mACCESS DENIED\033[0m\n");
         }
     } while(strcmp(login, "Joshua") != 0);
 
-    printf("in");
-
+    print_blue("GREETINGS PROFESSOR FALKEN.");
 }
